a89f1f56b012dd6c26696071cad351b04a14ac42
okitz <41997751+okitz@users.noreply.github.com>
2025-11-21 20:59:41 +0900

    init

---
+++ src/config.py
@@ -0,0 +1,6 @@
+# 対象自治体
+TARGET_CITIES = [
+    {"id": "13105", "name": "文京区", "prefecture": "東京都"},
+    {"id": "13106", "name": "台東区", "prefecture": "東京都"},
+    {"id": "47201", "name": "那覇市", "prefecture": "沖縄県"}
+]
